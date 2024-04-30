97c0c47eea2802286e79a656df9ba4c934961e6a
Ayyappa Reddy <areddy@talentquest.com>
2024-04-30 14:13:32 +0530

    revomed unwanted code from program file.

--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -9,31 +9,19 @@ class Program
 
     public static string SayHello(string name)
         => "Hello" + name;
+
     public static void Main(string[] args)
     {
-
-        // non-static methods call using create instantiation of class 
         Program program = new Program();
-        //program.Addition(1, 2);
-
+       
         AddNumberDelegate numberDelegate = new AddNumberDelegate(program.Addition);
-        //First way
+        
         numberDelegate(20, 60);
-
-        //Second way
         numberDelegate.Invoke(56, 78);
-
-        //Always static methods call using class
-        //string str = Program.SayHello("Ayyappa"); 
-
-        //Console.WriteLine(str);
-        //Console.ReadLine();
 
         SayHelloDelegate sayHelloDelegate = new SayHelloDelegate(SayHello);
 
-        //First way
         string str = sayHelloDelegate("ayyappa");
-        //Second way
         string str1 = sayHelloDelegate.Invoke("Reddy");
         Console.WriteLine(str);
         Console.ReadLine();
