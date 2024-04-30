9a85f1112d9f18f8cba5a739aff1734ee6b4e4fc
Ayyappa Reddy <areddy@talentquest.com>
2024-04-30 14:02:10 +0530

    Add project files.

---
+++ EventsAndDelegates/EventsAndDelegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class EventsAndDelegates
+    {
+        public EventsAndDelegates()
+        {
+            
+        }
+        public int Addition(int a, int b)
+            => a + b;
+
+
+        public static void Print1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+}
---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,43 @@
+﻿public delegate void AddNumberDelegate(int x, int y);
+public delegate string SayHelloDelegate(string name);
+
+class Program
+{
+    public void Addition(int a, int b)
+        => Console.WriteLine(a + b);
+    
+
+    public static string SayHello(string name)
+        => "Hello" + name;
+    public static void Main(string[] args)
+    {
+
+        // non-static methods call using create instantiation of class 
+        Program program = new Program();
+        //program.Addition(1, 2);
+
+        AddNumberDelegate numberDelegate = new AddNumberDelegate(program.Addition);
+        //First way
+        numberDelegate(20, 60);
+
+        //Second way
+        numberDelegate.Invoke(56, 78);
+
+        //Always static methods call using class
+        //string str = Program.SayHello("Ayyappa"); 
+
+        //Console.WriteLine(str);
+        //Console.ReadLine();
+
+        SayHelloDelegate sayHelloDelegate = new SayHelloDelegate(SayHello);
+
+        //First way
+        string str = sayHelloDelegate("ayyappa");
+        //Second way
+        string str1 = sayHelloDelegate.Invoke("Reddy");
+        Console.WriteLine(str);
+        Console.ReadLine();
+
+
+    }
+}
